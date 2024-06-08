c91374c1b1a0284935ac682653cd10b251a46ecd
Chingis <isabaevchingis@gmail.com>
2024-06-08 17:22:25 +0500

    pick solution

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,18 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
-
+  return Object.entries(obj)
+    .filter(([key]) => fields.includes(key))
+    .reduce((acc, [key, value]) => {
+      acc[key] = value;
+      return acc;
+    }, {});
 };
+
+const fruits = {
+  apple: 2,
+  orange: 4,
+  banana: 3
+};
+
+console.log(pick(fruits, 'apple', 'banana')); // { apple: 2, banana: 3 }
