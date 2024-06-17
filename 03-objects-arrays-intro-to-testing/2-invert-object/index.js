3673ac3feb9eaee7fffa5ccbb1aec360b3a39206
Chingis <isabaevchingis@gmail.com>
2024-06-17 17:46:19 +0500

    invertedObj solution

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,21 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+  if (!obj) {
+    return;
+  }
 
+  const invertedObj = {};
+
+  Object.entries(obj).forEach(([key, value]) => {
+    if (isPrimitive(value)) {
+      invertedObj[value] = key;
+    }
+  });
+
+  return invertedObj;
+}
+
+function isPrimitive(value) {
+  return Object(value) !== value;
 }
