5ef4701830229d01057b6958df072199237df88d
Chingis <isabaevchingis@gmail.com>
2024-06-18 19:22:19 +0500

    trim symbols solution

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,29 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === 0 && !string) {
+    return '';
+  }
 
+  if (size === undefined) {
+    return string;
+  }
+
+  let result = '';
+  let currentChar = '';
+  let count = 0;
+
+  for (const char of string) {
+    if (char === currentChar) {
+      count += 1;
+    } else {
+      currentChar = char;
+      count = 1;
+    }
+
+    if (count <= size) {
+      result += char;
+    }
+  }
+  return result;
 }
